47698f3c967d2503cea03a8d483c3ef4fa084c1e
fbenitez3 <fbenitez@stratechcorp.com>
2024-09-13 16:55:55 -0300

    set cors

--- medusa-config.js
+++ medusa-config.js
@@ -22,11 +22,10 @@ try {
 } catch (e) { }
 
 // CORS when consuming Medusa from admin
-const ADMIN_CORS =
-  process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001";
+const ADMIN_CORS = process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001,https://muebleriaezequiel-medusa-admin-jjqtpw5qt.vercel.app";
 
 // CORS to avoid issues when consuming Medusa from a client
-const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
+const STORE_CORS = process.env.STORE_CORS || "https://muebleria-ezequiel-front.vercel.app";
 
 const DATABASE_URL =
   process.env.DATABASE_URL || "postgres://localhost/medusa-store";
